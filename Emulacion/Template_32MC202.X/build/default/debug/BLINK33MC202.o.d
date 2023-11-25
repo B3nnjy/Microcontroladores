0ed42d5a22eb5f12ffa5fde10ab99c2acdddd9d6
Benjamin.n <benji06199@gmail.com>
2023-11-25 17:37:05 -0600

    Restringir archivos

--- Emulacion/Template_32MC202.X/build/default/debug/BLINK33MC202.o.d
+++
@@ -1,3 +0,0 @@
-build/default/debug/BLINK33MC202.o: \
- D:/Program\ Files/Microchip/MPLABX/v6.15/packs/Microchip/dsPIC33F-GP-MC_DFP/1.3.64/xc16/support\..\support\dsPIC33F\inc/p33FJ32MC202.inc \
- BLINK33MC202.s
--- Fisico/template.X/build/default/debug/mainasm33.o.d
+++
@@ -1,3 +0,0 @@
-build/default/debug/mainasm33.o: \
- D:/Program\ Files/Microchip/MPLABX/v6.15/packs/Microchip/dsPIC33F-GP-MC_DFP/1.3.64/xc16/support\..\support\dsPIC33F\inc/p33FJ128MC802.inc \
- mainasm33.s
